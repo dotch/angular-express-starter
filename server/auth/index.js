68172672b87930fa87141ceec027a7fa0abb884d
dotch <christian.x.weiss@gmail.com>
2015-05-17 17:25:36 -0700

    updated twitter oauth

--- server/auth/index.js
+++ server/auth/index.js
@@ -449,12 +449,13 @@ router.post('/yahoo', function(req, res) {
  | Login with Twitter
  |--------------------------------------------------------------------------
  */
-router.get('/twitter', function(req, res) {
+app.post('/auth/twitter', function(req, res) {
   var requestTokenUrl = 'https://api.twitter.com/oauth/request_token';
   var accessTokenUrl = 'https://api.twitter.com/oauth/access_token';
-  var authenticateUrl = 'https://api.twitter.com/oauthenticate';
+  var profileUrl = 'https://api.twitter.com/1.1/users/show.json?screen_name=';
 
-  if (!req.query.oauth_token || !req.query.oauth_verifier) {
+  // Part 1/2: Initial request from Satellizer.
+  if (!req.body.oauth_token || !req.body.oauth_verifier) {
     var requestTokenOauth = {
       consumer_key: config.TWITTER_KEY,
       consumer_secret: config.TWITTER_SECRET,
@@ -464,58 +465,73 @@ router.get('/twitter', function(req, res) {
     // Step 1. Obtain request token for the authorization popup.
     request.post({ url: requestTokenUrl, oauth: requestTokenOauth }, function(err, response, body) {
       var oauthToken = qs.parse(body);
-      var params = qs.stringify({ oauth_token: oauthToken.oauth_token });
 
-      // Step 2. Redirect to the authorization screen.
-      res.redirect(authenticateUrl + '?' + params);
+      // Step 2. Send OAuth token back to open the authorization screen.
+      res.send(oauthToken);
     });
   } else {
+    // Part 2/2: Second request after Authorize app is clicked.
     var accessTokenOauth = {
       consumer_key: config.TWITTER_KEY,
       consumer_secret: config.TWITTER_SECRET,
-      token: req.query.oauth_token,
-      verifier: req.query.oauth_verifier
+      token: req.body.oauth_token,
+      verifier: req.body.oauth_verifier
     };
 
     // Step 3. Exchange oauth token and oauth verifier for access token.
-    request.post({ url: accessTokenUrl, oauth: accessTokenOauth }, function(err, response, profile) {
-      profile = qs.parse(profile);
+    request.post({ url: accessTokenUrl, oauth: accessTokenOauth }, function(err, response, accessToken) {
 
-      // Step 4a. Link user accounts.
-      if (req.headers.authorization) {
-        User.findOne({ twitter: profile.user_id }, function(err, existingUser) {
-          if (existingUser) {
-            return res.status(409).send({ message: 'There is already a Twitter account that belongs to you' });
-          }
-          var token = req.headers.authorization.split(' ')[1];
-          var payload = jwt.decode(token, config.TOKEN_SECRET);
-          User.findById(payload.sub, function(err, user) {
-            if (!user) {
-              return res.status(400).send({ message: 'User not found' });
+      accessToken = qs.parse(accessToken);
+
+      var profileOauth = {
+        consumer_key: config.TWITTER_KEY,
+        consumer_secret: config.TWITTER_SECRET,
+        oauth_token: accessToken.oauth_token
+      };
+
+      // Step 4. Retrieve profile information about the current user.
+      request.get({ url: profileUrl + accessToken.screen_name, oauth: profileOauth, json: true }, function(err, response, profile) {
+
+        // Step 5a. Link user accounts.
+        if (req.headers.authorization) {
+          User.findOne({ twitter: profile.id }, function(err, existingUser) {
+            if (existingUser) {
+              return res.status(409).send({ message: 'There is already a Twitter account that belongs to you' });
             }
-            user.twitter = profile.user_id;
-            user.displayName = user.displayName || profile.screen_name;
-            user.save(function(err) {
+
+            var token = req.headers.authorization.split(' ')[1];
+            var payload = jwt.decode(token, config.TOKEN_SECRET);
+
+            User.findById(payload.sub, function(err, user) {
+              if (!user) {
+                return res.status(400).send({ message: 'User not found' });
+              }
+
+              user.twitter = profile.id;
+              user.displayName = user.displayName || profile.name;
+              user.picture = user.picture || profile.profile_image_url.replace('_normal', '');
+              user.save(function(err) {
+                res.send({ token: auth.createToken(user) });
+              });
+            });
+          });
+        } else {
+          // Step 5b. Create a new user account or return an existing one.
+          User.findOne({ twitter: profile.id }, function(err, existingUser) {
+            if (existingUser) {
+              return res.send({ token: auth.createToken(existingUser) });
+            }
+
+            var user = new User();
+            user.twitter = profile.id;
+            user.displayName = profile.name;
+            user.picture = profile.profile_image_url.replace('_normal', '');
+            user.save(function() {
               res.send({ token: auth.createToken(user) });
             });
           });
-        });
-      } else {
-        // Step 4b. Create a new user account or return an existing one.
-        User.findOne({ twitter: profile.user_id }, function(err, existingUser) {
-          if (existingUser) {
-            var token = auth.createToken(existingUser);
-            return res.send({ token: token });
-          }
-          var user = new User();
-          user.twitter = profile.user_id;
-          user.displayName = profile.screen_name;
-          user.save(function() {
-            var token = auth.createToken(user);
-            res.send({ token: token });
-          });
-        });
-      }
+        }
+      });
     });
   }
 });
