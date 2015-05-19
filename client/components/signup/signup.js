0a3b8e447cbe6802112b4ddf7e50c1b59752baf4
dotch <christian.x.weiss@gmail.com>
2015-05-19 16:21:05 -0700

    fixed signup page

--- client/components/signup/signup.js
+++ client/components/signup/signup.js
@@ -1,28 +1,45 @@
 angular.module('myApp.signup', [])
   .controller('SignupController', function($scope, $mdToast, $auth) {
+
+    $scope.authenticate = function(provider) {
+      $auth.authenticate(provider)
+        .then(loginSuccessHandler)
+        .catch(loginErrorHandler);
+    };
+
     $scope.signup = function() {
       $auth.signup({
         displayName: $scope.displayName,
         email: $scope.email,
         password: $scope.password
-      }).catch(function(response) {
-        if (typeof response.data.message === 'object') {
-          angular.forEach(response.data.message, function(message) {
-            $mdToast.show(
-              $mdToast.simple()
-                .content(message[0])
-                .position('bottom right')
-                .hideDelay(3000)
-            );
-          });
-        } else {
+      }).catch(loginErrorHandler);
+    };
+
+    function loginSuccessHandler() {
+      $mdToast.show(
+        $mdToast.simple()
+          .content('You have successfully logged in')
+          .position('bottom right')
+          .hideDelay(3000)
+      );
+    }
+    function loginErrorHandler(response) {
+      if (typeof response.data.message === 'object') {
+        angular.forEach(response.data.message, function(message) {
           $mdToast.show(
             $mdToast.simple()
-              .content(response.data.message)
+              .content(message[0])
               .position('bottom right')
               .hideDelay(3000)
           );
-        }
-      });
-    };
+        });
+      } else {
+        $mdToast.show(
+          $mdToast.simple()
+            .content(response.data ? response.data.message : response)
+            .position('bottom right')
+            .hideDelay(3000)
+        );
+      }
+    }
   });
