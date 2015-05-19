e478201979b102f62372cf6a830367ed0a05c485
dotch <christian.x.weiss@gmail.com>
2015-05-19 14:40:21 -0700

    remove some oauth provider fragments

--- client/app.js
+++ client/app.js
@@ -64,32 +64,9 @@ angular.module('myApp', [
     $authProvider.google({
       clientId: '631036554609-v5hm2amv4pvico3asfi97f54sc51ji4o.apps.googleusercontent.com'
     });
-
-    $authProvider.github({
-      clientId: '45ab07066fb6a805ed74'
-    });
-
-    $authProvider.linkedin({
-      clientId: '77cw786yignpzj'
-    });
-
-    $authProvider.yahoo({
-      clientId: 'dj0yJmk9SDVkM2RhNWJSc2ZBJmQ9WVdrOWIzVlFRMWxzTXpZbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD0yYw--'
-    });
-
+    
     $authProvider.twitter({
       url: '/auth/twitter'
     });
 
-    $authProvider.live({
-      clientId: '000000004C12E68D'
-    });
-
-    $authProvider.oauth2({
-      name: 'foursquare',
-      url: '/auth/foursquare',
-      clientId: 'MTCEJ3NGW2PNNB31WOSBFDSAD4MTHYVAZ1UKIULXZ2CVFC2K',
-      redirectUri: window.location.origin || window.location.protocol + '//' + window.location.host,
-      authorizationEndpoint: 'https://foursquare.com/oauth2/authenticate'
-    });
   });
