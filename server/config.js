25f92c74fbdf9f3ce1a283eb5391682aa1491297
dotch <christian.x.weiss@gmail.com>
2015-05-21 14:32:38 -0700

    heroku config

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   TOKEN_SECRET: process.env.TOKEN_SECRET || 'A hard to guess string',
-  MONGO_URI: process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME || process.env.MONGO_URI || 'localhost',
+  MONGO_URI: process.env.MONGOLAB_URI || process.env.MONGO_URI || 'localhost',
   FACEBOOK_SECRET: process.env.FACEBOOK_SECRET || 'Facebook Secret',
   GOOGLE_SECRET: process.env.GOOGLE_SECRET || 'Google Secret'
 };
